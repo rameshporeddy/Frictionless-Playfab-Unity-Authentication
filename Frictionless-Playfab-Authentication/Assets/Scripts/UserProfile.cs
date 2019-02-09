7ba52debffad5d1cdd57362c4d4ec8c4608f37c7
Shashir <Ramesh.poreddy@gmail.com>
2019-02-10 01:51:36 +0530

    First Commit

---
+++ Frictionless-Playfab-Authentication/Assets/Scripts/UserProfile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "RP/UserProfile")]
+public class UserProfile : ScriptableObject
+{
+    public string email;
+    public string displayname;
+    public string password;
+
+}
